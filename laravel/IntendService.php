df7e940b952ae2de77bcb1b6a854f38a417f32e6
Akbarali <Akbarali9705@mail.ru>
2022-10-29 18:22:05 +0500

    add order creqate

--- laravel/IntendService.php
+++ laravel/IntendService.php
@@ -14,10 +14,10 @@ namespace App\Services;
  */
 /*
  * Loyiha laravelda bo`lsa envga yozingiz kerak
-INTEND_API_KEY=<INTEND_BERGAN_API_KEY>
-INTEND_CALCULATE_URL=https://pay.intend.uz/api/v1/front/calculate-all
-INTEND_URL=https://pay.intend.uz
-INTEND_ORDER_CHECK_URL=https://pay.intend.uz/api/v1/external/order/check
+ * INTEND_API_KEY=<INTEND_BERGAN_API_KEY>
+ * INTEND_CALCULATE_URL=DOKEMENTATSIYADA BERILGANI
+ * INTEND_URL=DOKEMENTATSIYADA BERILGANI
+ * INTEND_ORDER_CHECK_URL=DOKEMENTATSIYADA BERILGANI
  */
 
 class IntendService
@@ -44,21 +44,26 @@ class IntendService
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, env('INTEND_CALCULATE_URL'));
         curl_setopt($ch, CURLOPT_POST, 1);                //0 for a get request
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(
-            [
+        curl_setopt(
+            $ch,
+            CURLOPT_POSTFIELDS,
+            json_encode(
                 [
-                    'api_key' => env('INTEND_API_KEY'),
-                    'price' => $product->price,
-                    'id' => $product->id,
+                    [
+                        'api_key' => env('INTEND_API_KEY'),
+                        'price'   => $product->price,
+                        'id'      => $product->id,
+                    ],
                 ]
-            ]
-        ));
+            )
+        );
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
         curl_setopt($ch, CURLOPT_TIMEOUT, 20);
         $response_json = curl_exec($ch);
         curl_close($ch);
         $response = json_decode($response_json, true);
+
         return $response['data']['items'][0]['prices'][0];
     }
 
@@ -75,11 +80,11 @@ class IntendService
         curl_setopt($ch, CURLOPT_POST, 1);                //0 for a get request
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['order_id' => $order_id]));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
             'Content-Type: application/json',
             'Accept: application/json',
-            'api-key: ' . env('INTEND_API_KEY')
-        ));
+            'api-key: '.env('INTEND_API_KEY'),
+        ]);
         $response_json = curl_exec($ch);
         curl_close($ch);
         $response = json_decode($response_json, true);
--- laravel/IntendServiceGuzzle.php
+++ laravel/IntendServiceGuzzle.php
@@ -19,9 +19,9 @@ use Illuminate\Support\Facades\Log;
 /*
  * Loyiha laravelda bo`lsa envga yozingiz kerak
  * INTEND_API_KEY=<INTEND_BERGAN_API_KEY>
- * INTEND_CALCULATE_URL=https://pay.intend.uz/api/v1/front/calculate-all
- * INTEND_URL=https://pay.intend.uz
- * INTEND_ORDER_CHECK_URL=https://pay.intend.uz/api/v1/external/order/check
+ * INTEND_CALCULATE_URL=DOKEMENTATSIYADA BERILGANI
+ * INTEND_URL=DOKEMENTATSIYADA BERILGANI
+ * INTEND_ORDER_CHECK_URL=DOKEMENTATSIYADA BERILGANI
  */
 
 class IntendServiceGuzzle
@@ -143,5 +143,166 @@ class IntendServiceGuzzle
         return false;
     }
 
+    public function createIntendOrder($token, $api_key, $order, $product, $supplier_id, $duration = 12)
+    {
+        $user           = auth()->user();
+        $product_price  = $product->price;
+        $product_id     = $product->id;
+        $product_name   = AdminService::trans($product->name);
+        $product_weight = $product->weight;
+        $product_sku    = $product->sku;
+        $product_url    = AdminService::trans($product->slug);
+        $used_bonus     = request()->input('used_bonus');
+        if (!$used_bonus) {
+            $used_bonus = 0;
+        }
+        $data = [
+            'duration'     => $duration,
+            'bonus_amount' => $used_bonus * 100,
+            'order_id'     => $supplier_id,
+            'redirect_url' => '',
+            'products'     => [
+                [
+                    'id'       => $product_id,
+                    'price'    => $product_price,
+                    'quantity' => $order->product_quantity,
+                    'name'     => $product_name,
+                    'sku'      => $product_sku,
+                    'url'      => route('web.product.slug', ['slug' => $product_url]),
+                    'weight'   => $product_weight,
+                ],
+            ],
+            'ref_id'       => $order->id,   // order id
+            'ttl'          => 600,
+        ];
+
+        try {
+            $response = $this->client->post($this->request_url.'/order/create', [
+                'headers' => [
+                    'Content-Type'  => 'application/json',
+                    'Accept'        => 'application/json',
+                    'Authorization' => 'Bearer '.$token,
+                    'api-key'       => $api_key,
+                ],
+                'json'    => $data,
+            ]);
+        } catch (\Exception $e) {
+            $token_new = (new \App\Services\IntendService())->getUserToken($user->phone, $api_key, true);
+
+            return (new IntendService())->createIntendOrder($token_new, $api_key, $order, $product, $supplier_id);
+        }
+        $json = (array)json_decode($response->getBody()->getContents(), true);
+        if (isset($json['success']) && $json['success'] === true) {
+            return $json['data'];
+        }
+
+        return false;
+    }
+
+    public function checkOrder($api_key, $token, $ref_id, $code)
+    {
+        $user = auth()->user();
+        try {
+            $response = $this->client->post($this->request_url.'/cheque/confirm', [
+                'headers' => [
+                    'Content-Type'  => 'application/json',
+                    'Accept'        => 'application/json',
+                    'api-key'       => $api_key,
+                    'Authorization' => 'Bearer '.$token,
+                ],
+                'json'    => [
+                    'ref_id' => $ref_id,
+                    'code'   => $code,
+                ],
+            ]);
+        } catch (\Exception $e) {
+            $token_new = (new \App\Services\IntendService())->getUserToken($user->phone, $api_key, true);
+
+            return (new IntendService())->checkOrder($api_key, $token_new, $ref_id, $code);
+        }
+
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        if (isset($json['success']) && $json['success'] === true) {
+            return [
+                'success' => true,
+                'data'    => [
+                    'message' => 'Buyurtma qabul qilindi tez orada bog`lanamiz',
+                ],
+            ];
+        }
+        $ref_id   = $this->reSendSms($api_key, $token, $ref_id);
+        $response = array_merge($json, ['ref_id' => $ref_id]);
+
+        return $json;
+    }
+
+    public function reSendSms($api_key, $token, $ref_id)
+    {
+        $user = auth()->user();
+        try {
+            $response = $this->client->post($this->request_url.'/cheque/resend', [
+                'headers' => [
+                    'Content-Type'  => 'application/json',
+                    'Accept'        => 'application/json',
+                    'api-key'       => $api_key,
+                    'Authorization' => 'Bearer '.$token,
+                ],
+                'json'    => [
+                    'ref_id' => $ref_id,
+                ],
+            ]);
+        } catch (\Exception $e) {
+            $token_new = (new \App\Services\IntendService())->getUserToken($user->phone, $api_key, true);
+
+            return (new IntendService())->reSendSms($api_key, $token_new, $ref_id);
+        }
+
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        if (isset($json['success']) && $json['success'] === true) {
+            return $json['data']['ref_id'];
+        }
+
+        return response()->json($json);
+    }
+
+    public function getUserToken($username, $api_key, $status = false, $user_id = false)
+    {
+        if ($status === false && session()->has('intend_token')) {
+            return session()->get('intend_token');
+        }
+        if ($status === true) {
+            session()->forget('intend_token');
+        }
+        try {
+            $response = $this->client->post($this->member_auth_url, [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept'       => 'application/json',
+                    'api-key'      => $api_key,
+                ],
+                'json'    => [
+                    'username' => $username,
+                    'token'    => hash('sha512', $username.'$'.$this->secret_key),
+                ],
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        if (isset($json['data']['token'])) {
+            session()->put('intend_token', $json['data']['token']);
+
+            if ($user_id !== false) {
+                return $json['data']['id'];
+            }
+
+            return $json['data']['token'];
+        }
+
+        return false;
+    }
 
 }
